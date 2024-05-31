883d049e8d0c66f8e6aaa25808cadd74e76fa2b0
NguyenDuyPhong1204 <nguyenduyphong12122004@gmail.com>
2024-05-31 21:09:31 +0700

    lab5

---
+++ app/src/main/java/com/example/lab4/Lab5/Lab5_1.kt
@@ -0,0 +1,21 @@
+package com.example.lab4.Lab5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lab4.R
+
+class Lab5_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lab51)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
